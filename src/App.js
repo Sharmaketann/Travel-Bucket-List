539ad458d53bea2c38675b969e71b121f15e42d5
Sharma Ketan <sharmaketann@gmail.com>
2022-06-20 20:14:58 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Card from './components/Card'
 import data from './data'
 import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   const cards = data.map((travel) => {
     return <Card key={travel.id} {...travel} />
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Header />
       <section className='cards--list'>{cards}</section>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer-copyright'>
+      <p>@2022 Travel Bucket List. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
